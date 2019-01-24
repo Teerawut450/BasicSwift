9e792443c304a8616ded316c5b353438d38722eb
Supasit.s <asd@com.com>
2019-01-24 11:25:26 +0700

    Loop (For Loop and While Loop)

---
+++ Loop.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+
+
+//Loop from array
+var numbers = [1,2,3,4,5]
+
+for myNumber in numbers {
+    print("My number ==> \(myNumber)")
+}
+
+
+var count: Int = 0
+while count <= 5 {
+    print("Count ==> \(count)")
+    count += 1
+    
+}
+
