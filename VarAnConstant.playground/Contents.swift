7d1f1f6b50898c0be91451829e755128139fcbff
Supasit.s <asd@com.com>
2019-01-24 09:41:03 +0700

    Var and let

---
+++ VarAnConstant.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+//Variable and Constant
+//Implicit Variable การประกาศตัวแปร
+var nameString = "Teerawut"
+var numberInt = 100
+var numberDou = 12.34
+var statusBol = true
+
+//Explicit Variable ประกาศแบบกำหนด Data Type
+var surname: String  = "123"
+var number2: Int = 120
+var number3: Double = 3.123
+var status2: Bool = false
+
+
+//Constant
+let myName: String = "Teerawut"
+
+nameString = "TeerawutK"
+//myName = "Teerawut450" Cannot Compile Error
+
+
+
